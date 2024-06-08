f720e6eed3bc7ab87935b15df1d087cd4241cf91
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-06-08 12:22:54 +0200

    update

--- components/BottomBar.tsx
+++ components/BottomBar.tsx
@@ -1,11 +1,31 @@
 "use client"
 
-import React from 'react'
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-function BottomBar() {
+const BottomBar = () => {
+  const pathname = usePathname();
+
   return (
-    <div>BottomBar</div>
-  )
-}
+    <div className="sticky flex bottom-0 z-20 w-full bg-dark-1 px-6 py-3 items-center justify-between md:hidden">
+      {sidebarLinks.map((link: any) => {
+        const isActive = pathname === link.route;
 
-export default BottomBar
+        return (
+          <Link
+            key={link.label}
+            href={link.route}
+            className={`flex gap-2 items-center rounded-lg py-2 px-4 ${
+              isActive && "bg-lime-400"
+            }`}
+          >
+            {link.icon} <p className="text-sm font-semibold text-light-1 max-sm:hidden">{link.label.split(/\s+/)[0]}</p>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default BottomBar;
